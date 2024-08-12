68c2a1907d066a93209b9d762a51b53ed4a05766
Iryna Abrosimova <iryna.abrosimova@cxdisruption.com>
2024-08-12 12:40:25 +0200

    add eventListener for players

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Fighting-Game
+
 fighting games. one with JavaScript and HTML canvas and another one with Kaplay(Kaboom) library
+
+1. Game using Vanilla JavaScript and Canvas
+   https://delilah07.github.io/Fighting-Game/Vanilla/
--- Vanilla/script.js
+++ Vanilla/script.js
@@ -6,13 +6,14 @@ canvas.height = 576;
 
 ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-const gravity = 0.2;
+const gravity = 0.7;
 
 class Sprite {
   constructor({ position, velocity, height = 150 }) {
     this.position = position;
     this.velocity = velocity;
     this.height = height;
+    this.lastKey;
   }
   draw() {
     ctx.fillStyle = 'red';
@@ -20,8 +21,9 @@ class Sprite {
   }
   update() {
     this.draw();
-    this.position.y += this.velocity.y;
+    this.position.x += this.velocity.x;
 
+    this.position.y += this.velocity.y;
     this.position.y + this.height + this.velocity.y >= canvas.height
       ? (this.velocity.y = 0)
       : (this.velocity.y += gravity);
@@ -43,12 +45,101 @@ const enemy = new Sprite({
   velocity: { x: 0, y: 0 },
 });
 
+const keys = {
+  a: {
+    pressed: false,
+  },
+  d: {
+    pressed: false,
+  },
+  w: {
+    pressed: false,
+  },
+  ArrowLeft: {
+    pressed: false,
+  },
+  ArrowRight: {
+    pressed: false,
+  },
+  ArrowUp: {
+    pressed: false,
+  },
+};
+
 function animate() {
   window.requestAnimationFrame(animate);
   ctx.fillStyle = 'black';
   ctx.fillRect(0, 0, canvas.width, canvas.height);
   player.update();
   enemy.update();
+
+  // player movements
+  player.velocity.x = 0;
+  if (keys.a.pressed && player.lastKey === 'a') {
+    player.velocity.x -= 5;
+  } else if (keys.d.pressed && player.lastKey === 'd') {
+    player.velocity.x += 5;
+  }
+
+  //enemy movements
+  enemy.velocity.x = 0;
+  if (keys.ArrowLeft.pressed && enemy.lastKey === 'ArrowLeft') {
+    enemy.velocity.x -= 5;
+  } else if (keys.ArrowRight.pressed && enemy.lastKey === 'ArrowRight') {
+    enemy.velocity.x += 5;
+  }
 }
 
 animate();
+
+window.addEventListener('keydown', (e) => {
+  switch (e.key) {
+    case 'd':
+      keys.d.pressed = true;
+      player.lastKey = 'd';
+      break;
+    case 'a':
+      keys.a.pressed = true;
+      player.lastKey = 'a';
+      break;
+    case 'w':
+      keys.w.pressed = true;
+      player.velocity.y = -20;
+      break;
+    case 'ArrowRight':
+      keys.ArrowRight.pressed = true;
+      enemy.lastKey = 'ArrowRight';
+      break;
+    case 'ArrowLeft':
+      keys.ArrowLeft.pressed = true;
+      enemy.lastKey = 'ArrowLeft';
+      break;
+    case 'ArrowUp':
+      keys.ArrowUp.pressed = true;
+      enemy.velocity.y = -20;
+      break;
+  }
+  console.log;
+});
+window.addEventListener('keyup', (e) => {
+  switch (e.key) {
+    case 'd':
+      keys.d.pressed = false;
+      break;
+    case 'a':
+      keys.a.pressed = false;
+      break;
+    case 'w':
+      keys.w.pressed = false;
+      break;
+    case 'ArrowRight':
+      keys.ArrowRight.pressed = false;
+      break;
+    case 'ArrowLeft':
+      keys.ArrowLeft.pressed = false;
+      break;
+    case 'ArrowUp':
+      keys.ArrowUp.pressed = false;
+      break;
+  }
+});
